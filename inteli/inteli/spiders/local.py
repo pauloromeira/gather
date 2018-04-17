9bc40b5cdeae6f0f925c2a5e7a9c4ae9b1fd6cd3
Paulo Romeira <paulo@pauloromeira.com>
2018-04-17 18:07:54 -0300

    Update site and spider

--- inteli/inteli/spiders/local.py
+++ inteli/inteli/spiders/local.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from scrapy.http import Request
+
 
 class LocalSpider(scrapy.Spider):
     name = "local"
@@ -10,4 +12,17 @@ class LocalSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-        import ipdb; ipdb.set_trace()
+        print('******** ' + response.xpath('//title/text()').extract_first())
+
+        link = response.xpath('//a/@href').extract_first()
+        yield Request(response.urljoin(link), callback=self.page1)
+
+    def page1(self, response):
+        print('******** ' + response.xpath('//title/text()').extract_first())
+
+        links = response.xpath('//a/@href').extract()
+        for link in links:
+            yield Request(response.urljoin(link), callback=self.other_pages)
+
+    def other_pages(self, response):
+        print('******** ' + response.xpath('//title/text()').extract_first())
