d14c3b17262ef5df842893318064602f96a9eeed
Paulo Romeira <paulo@pauloromeira.com>
2018-04-23 18:53:15 -0300

    Add scrapy-inline-requests to requirements

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 git clone -b '1.1.3' https://github.com/scrapy/scrapy.git packages/scrapy
 git clone -b 'twisted-16.4.1' https://github.com/twisted/twisted.git packages/twisted
+git clone https://github.com/rmax/scrapy-inline-requests packages/scrapy-inline-requests
 
 pyenv uninstall gather
 pyenv virtualenv 2.7.14 gather
@@ -7,4 +8,5 @@ pyenv local gather
 
 pip install -e packages/twisted
 pip install -e packages/scrapy
+pip install -e packages/scrapy-inline-requests
 pip install -r requirements.txt
