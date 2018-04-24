2acf8714d149e8dd0750cd219cc4780a49179697
Paulo Romeira <paulo@pauloromeira.com>
2018-04-23 23:16:40 -0300

    Modify init to get shallow clones

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-git clone -b '1.1.3' https://github.com/scrapy/scrapy.git packages/scrapy
-git clone -b 'twisted-16.4.1' https://github.com/twisted/twisted.git packages/twisted
-git clone https://github.com/rmax/scrapy-inline-requests packages/scrapy-inline-requests
+git clone --depth=1 -b '1.1.3' --single-branch https://github.com/scrapy/scrapy.git packages/scrapy
+git clone --depth=1 -b 'twisted-16.4.1' --single-branch https://github.com/twisted/twisted.git packages/twisted
+git clone --depth=1 -b 'master' --single-branch https://github.com/rmax/scrapy-inline-requests packages/scrapy-inline-requests
 
 pyenv uninstall gather
 pyenv virtualenv 2.7.14 gather
