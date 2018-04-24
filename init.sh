1576d442d218a2117b650ad8d9195d3d874cc359
Paulo Romeira <paulo@pauloromeira.com>
2018-04-24 00:07:22 -0300

    Change remote to my fork of inline-requests

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 git clone --depth=1 -b '1.1.3' --single-branch https://github.com/scrapy/scrapy.git packages/scrapy
 git clone --depth=1 -b 'twisted-16.4.1' --single-branch https://github.com/twisted/twisted.git packages/twisted
-git clone --depth=1 -b 'master' --single-branch https://github.com/rmax/scrapy-inline-requests packages/scrapy-inline-requests
+git clone https://pauloromeira@github.com/pauloromeira/scrapy-inline-requests packages/scrapy-inline-requests
 
 pyenv uninstall gather
 pyenv virtualenv 2.7.14 gather
